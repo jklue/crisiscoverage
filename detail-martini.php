6ac8f9d9822ad1c7505883da7982f3bfb51a8397
lukehatfield <luke@lukehatfield.com>
2014-04-26 10:19:36 -0400

    martini css updates

--- detail-martini.php
+++ detail-martini.php
@@ -6,17 +6,16 @@
 
 	<div class="content">
 
-		<nav class="martini">
-			<ul>
-				<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
-				<li><a href="/project-data-martini" class="right arrow">Data<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
-			</ul>
-		</nav>
-
 		<!-- Tutorial -->
 		<div class="tutorial-wrapper">
 			<div class="tutorial">
 				<p>So this is how you use this graph...</p>
+				<nav class="martini">
+					<ul>
+						<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
+						<li><a href="/project-data-martini" class="right arrow">Data<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
+					</ul>
+				</nav>
 			</div>
 		</div>
 
--- timelinemartini.php
+++ timelinemartini.php
@@ -8,11 +8,13 @@
 		<!-- Tutorial -->
 		<div class="tutorial-wrapper">
 			<div class="tutorial">
+				<h1>The Timeline</h1>
 				<p>So this is how you use this graph...</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<nav class="martini">
 					<ul>
 						<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
-						<li><a href="/detail-martini" class="right arrow">Details<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
+						<li><a href="/detail-martini" class="right arrow">Next<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
 					</ul>
 				</nav>
 			</div>
