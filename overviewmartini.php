d550cffafb593c4312971994a5f3e0686102e10a
lukehatfield <luke@lukehatfield.com>
2014-04-26 16:53:25 -0400

    crisis selector pushed right. Martini css tweaked. y axis working on timeline

--- overviewmartini.php
+++ overviewmartini.php
@@ -3,25 +3,28 @@
 <body id="overview">
 	<?php include 'includes/martini-header.html'; ?>
 
-	<!-- Tutorial -->
-	<div class="tutorial-wrapper">
-		<div class="tutorial">
-			<h1>The Overview</h1>
-			<p>So this is how you use this graph...</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<nav class="martini">
-				<ul>
-					<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
-					<li><a href="/timelinemartini" class="right arrow">Next<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
-				</ul>
-			</nav>
+	<div class="content">
+
+		<!-- Tutorial -->
+		<div class="tutorial-wrapper">
+			<div class="tutorial">
+				<h1>The Overview</h1>
+				<p>So this is how you use this graph...</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<nav class="martini">
+					<ul>
+						<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
+						<li><a href="/timelinemartini" class="right arrow">Next<img src="images/icons/rightarrow.png" alt="Right Arrow" title="Learn More"></a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
+
+		<!-- Overview Vis -->
+		<div id="overviewVisGIF" class="cf"></div>
+
 	</div>
-
-	<!-- Overview Vis -->
-	<div id="overviewVisGIF" class="cf"></div>
-
-<!-- footer -->
+	<!-- footer -->
 	<?php include 'includes/footer.html'; ?>
 </body>
 </html>
