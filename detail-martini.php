9e806e2b9d06ff78e0f55288199024413bf122a5
lukehatfield <lukejayhatfield@gmail.com>
2014-05-01 23:04:11 -0400

    details martini updated

--- detail-martini.php
+++ detail-martini.php
@@ -10,7 +10,8 @@
 		<div class="tutorial-wrapper cf">
 			<div class="tutorial">
 			<h1>Details</h1>
-				<p>Details info here...</p>
+				<p>The 'Details' Page gives a more in depth comparison of results from each site by crisis. The primary visualization is a stacked bar chart providing results from each of our tracked sites over the course of the crisis lifespan. On hover, aa tooltip is presented with the total crisis coverage article results as well as the baseline coverage given by that site for any news and then the relative percentage.</p>
+				<p>Clicking on the bars reveals a secondary interactive table that shows sample article results for the crisis within the month selected. This gives a taste of the articles our custom query returned, and is limited to 10 articles.</p>
 				<nav class="martini">
 					<ul>
 						<!-- <li><a href="/overview" class="left arrow"><img src="images/icons/leftarrow.png" alt="Left Arrow" title="Skip to Charts">Skip to Charts</a></li> -->
