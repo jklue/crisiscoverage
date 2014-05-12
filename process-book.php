9a05a128858200eaa4e85796a30a318a1b92fe4b
michaeljohns <michaeljohns@g.harvard.edu>
2014-05-12 07:42:55 -0400

    auto

--- process-book.php
+++ process-book.php
@@ -1,6 +1,6 @@
 <?php include 'includes/head.html'; ?>
 
-<body id="timeline">
+<body id="process-book">
   <?php include 'includes/header.html'; ?>
 
   <style>
@@ -9,27 +9,25 @@
   }
   </style>
   <div class="content cf">
-   <header class="tab_header">
-      <div class="overview_title">Process Book And Related</div>
-      <div class="tip">PDF and  Markdown formats</div>
-   </header>
+    <header class="tab_header">
+       <div class="overview_title">Process Book And Related</div>
+       <div class="tip">PDF and  Markdown(MD) formats, if problem with links use MD.</div>
+    </header>
 
-  <div id="process_main">
-  <ul>
-  <li>Process Book <a href="assets/process-book.pdf">(PDF)</a> <a href="process-book.md">(MD)</a></li>
-  <li>Design Studio Feedback <a href="assets/design_studio_feedback.pdf">(PDF)</a> <a
-  href="assets/design_studio_feedback.md">
-  (MD)</a></li>
-  <li>Google API Concept <a href="assets/google-api-concept.pdf">(PDF)</a> <a href="assets/google-api-concept.md">(MD)
-  </a></li>
-  <li>Data Processing Overview <a href="assets/data-overview.pdf">(PDF)</a> <a
-  href="productiondata/data-overview.md">(MD)</a></li>
-  <li>Data Processing Historical<a href="assets/process-data-history.pdf">(PDF)</a> <a
-  href="assets/process-data-history.md">(MD)</a></li>
-
-  </ul>
-  </div>
-
+    <div id="process_main">
+       <ul>
+           <li>Process Book <a href="assets/process-book.pdf">(PDF)</a> <a href="process-book.md">(MD)</a></li>
+           <li>Design Studio Feedback <a href="assets/design_studio_feedback.pdf">(PDF)</a> <a
+           href="assets/design_studio_feedback.md">
+           (MD)</a></li>
+           <li>Google API Concept <a href="assets/google-api-concept.pdf">(PDF)</a> <a href="assets/google-api-concept.md">(MD)
+           </a></li>
+           <li>Data Processing Overview <a href="assets/data-overview.pdf">(PDF)</a> <a
+           href="productiondata/data-overview.md">(MD)</a></li>
+           <li>Historical Data Processing <a href="assets/process-data-history.pdf">(PDF)</a> <a
+           href="assets/process-data-history.md">(MD)</a></li>
+       </ul>
+    </div>
   </div>
 
 <!-- footer -->
